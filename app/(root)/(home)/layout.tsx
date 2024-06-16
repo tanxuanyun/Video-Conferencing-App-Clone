72498747901d749ab36c2ff6f58b170bd738c23d
tanxuanyun <tanxuanyun42@gmail.com>
2024-06-16 23:06:12 +0800

    Add authentication

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,11 +1,14 @@
 import React,{ReactNode} from 'react'
 
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+
 const HomeLayout = ({children}:{children:ReactNode}) => {
   return (
     <main className='relative'>
-        Navbar
+        <Navbar/>
         <div className='flex'>
-            Sidebar
+            <Sidebar/>
             <section className="flex min-h-screen flex-1 flex-col px-6 pd-6 pt-28 max-md:pd-14 sm:px-14">
                 <div className="w-full">
                     {children}
